dba1944296fd000633b3350fd080ccccb81701f2
Sprevon <leon.bwchen@foxmail.com>
2025-01-04 11:29:29 +0800

    first init

---
+++ src/test/java/net/leonbwchen/smartcrack/SmartCrackApplicationTests.java
@@ -0,0 +1,13 @@
+package net.leonbwchen.smartcrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
